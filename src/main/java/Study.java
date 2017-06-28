d31af1ba02252d3000a62163022a0fd30d278f5d
dasinWalk <sunkangqing@163.com>
2017-06-28 14:04:37 +0800

    Initial commit

---
+++ src/main/java/Study.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/6/27.
+ */
+public class Study {
+    public static void main(String args[]){
+        System.out.println(1);
+        int temp1 = 50;
+        int temp2 = 100;
+        int temp3 = addNum(temp1,temp2);
+        System.out.println("====="+temp3);
+        Map map = new HashMap();
+        map.put(1,1);
+    }
+
+    private static int addNum(int temp1, int temp2) {
+        int temp3 = temp1+temp2;
+        return temp3;
+    }
+}
