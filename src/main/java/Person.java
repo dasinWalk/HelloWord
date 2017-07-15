a5ebc511f43c37bcfadea1fccd262a22949946b7
dasinWalk <sunkangqing@163.com>
2017-07-15 17:40:10 +0800

    上传了个新文件

---
+++ src/main/java/Person.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+public class Person {
+    private String name;
+    public Person(){
+       this("kangqing");
+    }
+    public Person(String name){
+        this.name = name;
+    }
+}
